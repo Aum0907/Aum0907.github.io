d496dca16a0c5b7472bca2b4d8f88d82306a6d64
Aum Patel <patelaum0907@gmail.com>
2025-05-08 00:34:28 -0500

    Bug fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'iOS >= 10'], // Add Safari and older browser targets
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'], // Include polyfills for async/await
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
